a28431fbaf46a73ec3caf1fc083f82301c5033f0
jemsley canela <jemscanela@gmail.com>
2018-05-30 19:43:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function theBeatlesPlay(array1,array2){
   var emptyArr ={};
   for(var i=0; i< array1.length; i++){
-    var sentence = 
-    emptyArr[i] = 
+    var sentence = array1[i] +" palys "+ array2[i];
+    emptyArr[i] = sentence
   }
   
   return emptyArr
