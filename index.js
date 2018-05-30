97bc90429c4709cf10bac712fd715ddd963f829e
jemsley canela <jemscanela@gmail.com>
2018-05-30 19:39:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(array1,array2){
+  var emptyArr ={};
+  for(var i=0; i< array1.length; i++){
+    emptyArr[i] = array[i] +"palys"+ array2[i]
+  }
+  
+  return emptyArr
+}
+
+function johnLennonFacts(array){
+  var i=0;
+  while(i<array.length){
+    array[i]= "${array[i]}!!!"
+    i++
+  }
+  
+  return array;
+}
+
+function iLoveTheBeatles(n){
+  var emptyArr ={};
+  
+  do{
+    emptyArr.push("Ilove the Beatles!")
+    n++
+  } while(n<15)
+}
